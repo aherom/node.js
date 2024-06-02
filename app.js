207c2c57e21b4604ba4cb0ad5b5304e59e9b368c
Om aher <aherom099@gmail.com>
2024-06-02 17:04:40 +0530

    response

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 const http = require("http");
 const server = http.createServer(
-    ((req,res)=>{
-             console.log("om Aher");
+    ((req,res)=>
+    {
+        if(req.url==='/home') 
+            {
+                res.write('<html><body>Welcome home</body></html>')   
+               
+            }  
+            
+            if(req.url==='/about') 
+                {
+                    res.write('<html><body> Welcome to About Us page</body></html>')   
+                   
+                }  
+                if(req.url==='/node') 
+                    {
+             res.write('<html><body>  Welcome to my Node Js project</body></html>')   
+                       
+                    }                
     })
+
 )
 
-server.listen(4000);
+server.listen(3000);
