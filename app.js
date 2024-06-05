d9d8a39c26770a4eaa682863158fa93299140d87
Om aher <aherom099@gmail.com>
2024-06-05 21:03:58 +0530

    Modified app.js, added message.txt and remot.js

--- app.js
+++ app.js
@@ -1,52 +1,5 @@
 const http = require("http")
-const fs = require('fs');
-const server = http.createServer(
-    (req,res)=>
-        {
-            if(req.url==="/")
-               {
-                if (fs.existsSync('message.txt')) {
-                    messages = fs.readFileSync('message.txt', 'utf-8');
-                  }
-            
-                
-                 
-                    messageHtml = `<p>${messages}</p>`;
-                 
-                  res.write(`<html>
-                    <body>
-                    ${messageHtml}<form action="/message" method="post">
-                           <input type="text" name="message"></input>
-                           <button type="submit">send</button>
-                          
-                        </form>
-                    </body>
-                  </html>`);
-                  return res.end();
-               }
-
-
-             if (req.url === '/message' && req.method === 'POST')
-             {
-                const body = [];
-                req.on('data', (chunk) => {
-                  console.log(chunk);
-                  body.push(chunk);
-                
-                })
-                req.on('end', () => {
-                    const parsedBody = Buffer.concat(body).toString();
-                    const message = parsedBody.split('=')[1];
-                    fs.writeFileSync('message.txt', message);
-                  });
-                  res.statusCode = 302;
-                  res.setHeader('Location', '/');
-                  return res.end();
-             }
-                
-                  
-           
-        }
-);
+const remote = require('./remot');
+const server = http.createServer(remote);
 server.listen(4000);
 
---
+++ remot.js
@@ -0,0 +1,49 @@
+const fs = require('fs');
+
+const func = (req,res)=>
+    {
+        if(req.url==="/")
+           {
+            if (fs.existsSync('message.txt')) {
+                messages = fs.readFileSync('message.txt', 'utf-8');
+              }
+        
+            
+             
+                messageHtml = `<p>${messages}</p>`;
+             
+              res.write(`<html>
+                <body>
+                ${messageHtml}<form action="/message" method="post">
+                       <input type="text" name="message"></input>
+                       <button type="submit">send</button>
+                      
+                    </form>
+                </body>
+              </html>`);
+              return res.end();
+           }
+
+
+         if (req.url === '/message' && req.method === 'POST')
+         {
+            let body ="";
+            req.on('data', (chunk) => {
+              
+                body += chunk.toString('utf-8');
+            
+            })
+            req.on('end', () => {
+                //const parsedBody = body.toString();
+                const message = body.split('=')[1];
+                fs.writeFileSync('message.txt', message);
+              });
+              res.statusCode = 302;
+              res.setHeader('Location', '/');
+              return res.end();
+         }
+            
+              
+       
+    }
+    module.exports=func;
